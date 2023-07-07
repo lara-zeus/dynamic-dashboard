4148fad566c2e41ce7eeb16081cbc8fcad0883d9
Ash Monsh <wh7r.com@gmail.com>
2023-07-07 04:39:06 +0300

    fix responsive class for columns layout

--- src/Models/Columns.php
+++ src/Models/Columns.php
@@ -16,11 +16,11 @@ class Columns extends Model
     public function getRows(): array
     {
         return [
-            ['key' => 'headerColumn', 'name' => __('top'), 'class' => 'w-full col-span-1 md:col-span-12'],
-            ['key' => 'rightColumn', 'name' => __('right'), 'class' => 'w-1/4 col-span-1 md:col-span-3'],
-            ['key' => 'middleColumn', 'name' => __('middle'), 'class' => 'w-2/4 col-span-1 md:col-span-6'],
-            ['key' => 'leftColumn', 'name' => __('left'), 'class' => 'w-1/4 col-span-1 md:col-span-3'],
-            ['key' => 'footerColumn', 'name' => __('bottom'), 'class' => 'w-full col-span-1 md:col-span-12'],
+            ['key' => 'headerColumn', 'name' => __('top'), 'class' => 'w-full col-span-12 md:col-span-12'],
+            ['key' => 'rightColumn', 'name' => __('right'), 'class' => 'w-1/4 col-span-12 md:col-span-3'],
+            ['key' => 'middleColumn', 'name' => __('middle'), 'class' => 'w-2/4 col-span-12 md:col-span-6'],
+            ['key' => 'leftColumn', 'name' => __('left'), 'class' => 'w-1/4 col-span-12 md:col-span-3'],
+            ['key' => 'footerColumn', 'name' => __('bottom'), 'class' => 'w-full col-span-12 md:col-span-12'],
         ];
     }
 }
