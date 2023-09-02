d131cb2350fa2d348e85bebf56c9c84af65906ae
Ash Monsh <wh7r.com@gmail.com>
2023-09-02 23:52:04 +0300

    fix width

--- src/Models/Columns.php
+++ src/Models/Columns.php
@@ -17,9 +17,9 @@ class Columns extends Model
     {
         return [
             ['key' => 'headerColumn', 'name' => __('top'), 'class' => 'w-full col-span-12 md:col-span-12'],
-            ['key' => 'rightColumn', 'name' => __('right'), 'class' => 'w-1/4 col-span-12 md:col-span-3'],
-            ['key' => 'middleColumn', 'name' => __('middle'), 'class' => 'w-2/4 col-span-12 md:col-span-6'],
-            ['key' => 'leftColumn', 'name' => __('left'), 'class' => 'w-1/4 col-span-12 md:col-span-3'],
+            ['key' => 'rightColumn', 'name' => __('right'), 'class' => 'w-full col-span-12 md:col-span-3'],
+            ['key' => 'middleColumn', 'name' => __('middle'), 'class' => 'w-full col-span-12 md:col-span-6'],
+            ['key' => 'leftColumn', 'name' => __('left'), 'class' => 'w-full col-span-12 md:col-span-3'],
             ['key' => 'footerColumn', 'name' => __('bottom'), 'class' => 'w-full col-span-12 md:col-span-12'],
         ];
     }
