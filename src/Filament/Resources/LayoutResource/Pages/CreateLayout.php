47a1340872ff069ef2ceda358ecec250ce4fe6d2
Ash Monsh <wh7r.com@gmail.com>
2025-12-10 01:25:36 +0300

    set user when creating a new layout

--- src/Filament/Resources/LayoutResource/Pages/CreateLayout.php
+++ src/Filament/Resources/LayoutResource/Pages/CreateLayout.php
@@ -8,4 +8,11 @@ use LaraZeus\DynamicDashboard\Filament\Resources\LayoutResource;
 class CreateLayout extends CreateRecord
 {
     protected static string $resource = LayoutResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->user()->id;
+
+        return parent::mutateFormDataBeforeCreate($data);
+    }
 }
